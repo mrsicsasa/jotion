de0afe0bc00bb87f272630bcb825b7b1db5cb8d5
mrsicsasa <sasamrsic20@gmail.com>
2023-12-04 11:21:16 +0100

    Authentication and Database

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from '@/components/providers/theme-provider'
+import { ConvexClientProvider } from '@/components/providers/convex-provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -33,7 +34,8 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider
+      <ConvexClientProvider>
+      <ThemeProvider
         attribute='class'
         defaultTheme='system'
         enableSystem
@@ -41,6 +43,7 @@ export default function RootLayout({
         storageKey='jotion-theme'>
           {children}
         </ThemeProvider>
+      </ConvexClientProvider>
         </body>
     </html>
   )
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default{
+    providers:[
+        {
+            domain:"https://sharp-martin-3.clerk.accounts.dev",
+            applicationID:"convex"
+        }
+    ]
+}
