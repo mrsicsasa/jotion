5f804f07849fb3581733b43135e7cf211ee69b0b
mrsicsasa <sasamrsic20@gmail.com>
2023-12-05 18:12:58 +0100

    sidebar beta

---
+++ app/(main)/_components/navigation.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { ChevronsLeft } from "lucide-react"
+import { ElementRef, useRef, useState } from "react";
+import { useMediaQuery } from "usehooks-ts";
+
+export const Navigation=()=>{
+    const isMobile=useMediaQuery("(max-width:768px)")
+    const isResizingRef=useRef(false);
+    const sidebarRef=useRef<ElementRef<"aside">>(null)
+    const navbarRef=useRef<ElementRef<"div">>(null);
+    const[isResetting,setIsResseting]=useState(false)
+    const[isCollapsed,setIsCollapsed]=useState(false)
+    return(
+      <>
+      <aside className="group/sidebar h-full bg-secondary 
+      overflow-y-auto relative flex w-60 flex-col z-[99999]">
+        <div role="button" 
+        className="h-6 w-6 text-muted-foreground rounded-sm
+        hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute
+        top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition">
+            <ChevronsLeft className="h-6 w-6"/>
+        </div>
+        <div>
+            <p>Action items</p>
+        </div>
+        <div className="mt-4">
+            <p>Documents</p>
+        </div>
+        <div className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0"/>
+      </aside>
+      </>
+    )
+}
