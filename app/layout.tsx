e3c06f4bd5776201f6f9cac8c3a02f859e7f7077
mrsicsasa <sasamrsic20@gmail.com>
2024-01-03 17:35:48 +0100

    Added schema

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from '@/components/providers/theme-provider'
 import { ConvexClientProvider } from '@/components/providers/convex-provider'
-
+import { Toaster } from 'sonner'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -41,6 +41,7 @@ export default function RootLayout({
         enableSystem
         disableTransitionOnChange
         storageKey='jotion-theme'>
+          <Toaster position='bottom-center'/>
           {children}
         </ThemeProvider>
       </ConvexClientProvider>
