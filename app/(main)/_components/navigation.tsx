eb9a960d6bdcd6fd5995c7ad2b43c20d06e7d8f6
mrsicsasa <sasamrsic20@gmail.com>
2024-01-04 23:16:08 +0100

    updated icon template and added search button

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { cn } from "@/lib/utils";
-import { ChevronsLeft, MenuIcon, PlusCircle } from "lucide-react"
+import { ChevronsLeft, MenuIcon, PlusCircle, Search } from "lucide-react"
 import { usePathname } from "next/navigation";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
@@ -123,6 +123,12 @@ export const Navigation=()=>{
             </div>
             <div>
               <UserItem/>
+              <Item
+              label="Search"
+              icon={Search}
+              isSearch
+              onClick={()=>{}}
+              />
               <Item onClick={handleCreate}label="New page"icon={PlusCircle}/>
             </div>
             <div className="mt-4">
