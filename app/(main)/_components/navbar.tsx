5172fc35f22f623ec5c4ab730ba2775ba5697a0b
mrsicsasa <sasamrsic20@gmail.com>
2024-01-13 18:21:30 +0100

    added banner for items in trash and menu for deleting item

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -5,6 +5,8 @@ import { useQuery } from "convex/react";
 import { MenuIcon } from "lucide-react";
 import { useParams } from "next/navigation";
 import { Title } from "./title";
+import { Banner } from "./banner";
+import { Menu } from "./menu";
 interface NavbarProps{
     isCollapsed:boolean;
     onResetWidth:()=>void
@@ -20,8 +22,11 @@ export const Navbar=({
     if(document===undefined){
         return(
             <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full 
-            flex items-center">
+            flex items-center justify-between">
                 <Title.Skeleton/>
+                <div className="flex items-center gap-x-2">
+                   <Menu.Skeleton/>
+                </div>
             </nav>
         )
     }
@@ -39,10 +44,16 @@ export const Navbar=({
                 className="h-4 w-4 text-muted-foreground"
                 />
             )}
-            <div className="flex items-center justify-between wf-full">
+            <div className="flex items-center justify-between w-full">
                 <Title initialData={document}/>
+                <div className="flex items-center gap-x-2">
+                   <Menu documentId={document._id}/>
+                </div>
             </div>
         </nav>
+        {document.isArchived &&(
+            <Banner documentId={document._id}/>
+        )}
         </>
     )
 }
